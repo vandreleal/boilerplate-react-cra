d16056db1afc26a5f8c76c0bf1d2d188a0a7667c
Vandre Leal <vandre.leal.candido@gmail.com>
2022-06-25 14:50:13 -0300

    build(deps): add boilerplate dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloProvider } from "@apollo/client/react"
+import { ThemeProvider } from "styled-components"
+
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import GlobalStyle from "./styles/global"
+
+const client = new ApolloClient({
+  uri: `${process.env.API_URL}/graphql`,
+  cache: new InMemoryCache({}),
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+
+// Define what main theme will look like
+const theme = {}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
