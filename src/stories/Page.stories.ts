db0fb418c45d9644b70658ffdedd2d53057a89d0
Vandré Leal <vandre.leal.candido@gmail.com>
2024-03-30 09:01:52 -0300

    build(deps): bump dependencies

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,19 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { userEvent, within } from "@storybook/testing-library"
+import { userEvent, within } from "@storybook/test"
 
 import { Page } from "./Page"
 
-const meta = {
+const meta: Meta<typeof Page> = {
   title: "Example/Page",
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
   },
-} satisfies Meta<typeof Page>
+}
 
 export default meta
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof Page>
 
 export const LoggedOut: Story = {}
 
